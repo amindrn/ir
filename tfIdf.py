5caa1eb1d245e1d348dd7b0774fc899bac411587
DESKTOP-VSNBHLJ\Amin <pourderakhshanmohammadamin@gmail.com>
2022-06-16 11:47:53 +0430

    clean tfIdf file

--- tfIdf.py
+++ tfIdf.py
@@ -10,28 +10,19 @@ csvreader = csv.reader(file)
 docs = []
 for row in csvreader:
     docs.append(row[2])
-# print(rows[100][2])
-words = []
-for d in docs:
-    words.extend(d.split())
-# print(type(words))
-# print(len(words))
-vocab = np.array(words)
-vocab = np.unique(vocab)
-# print(len(vocab))
-# print(vocab[100])
+# words = []
+# for d in docs:
+#     words.extend(d.split())
+# vocab = np.array(words)
+# vocab = np.unique(vocab)
 vectorizer = TfidfVectorizer(lowercase=True)
-x = vectorizer.fit(docs)
+vectorizer.fit(docs)
 tfidf_docs = vectorizer.fit_transform(docs)
-# print(x)
-y = vectorizer.vocabulary_
-# print(y)
+vectorizer.vocabulary_
 docs_tfidf = vectorizer.transform(docs)
 type(docs_tfidf), docs_tfidf.shape
 z = docs_tfidf[0].A
-# print(z)
 def quaryPro(query):
-    # query = 'قطعه سازان و طی یک نقشه راه دو ساله'
     tfidf_query = vectorizer.transform([query])[0]
     cosines = []
     for d in tqdm(tfidf_docs):
@@ -40,14 +31,12 @@ def quaryPro(query):
     sorted_ids = np.argsort(cosines)
     for i in range(k):
         cur_id = sorted_ids[-i-1]
-        print(docs[cur_id], cosines[cur_id])
+        print('----------------------------------------------',cosines[cur_id],'--------------------------------------------------------')
+        print(docs[cur_id])
 while True:
-    print('وارد کنید')
+    print('###################################################وارد کنید','################################################')
     query = input()
     quaryPro(query)
     print('----------------------------------------------------------------------------------------------------------------')
 
 
-
-# vocab = set(words)
-# print(vocab[3])
