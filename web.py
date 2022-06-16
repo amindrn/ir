153f2122997f58e85a6fc1bcc2e0462e7ae27110
DESKTOP-VSNBHLJ\Amin <pourderakhshanmohammadamin@gmail.com>
2022-06-16 11:18:42 +0430

    clean web.py file

--- web.py
+++ web.py
@@ -12,12 +12,10 @@ def scrapAll():
     pagesNum = input('number of pages:')
     fileName = input('csv file name(dont enter .csv):')
     while True:
-
         page_url = f'https://www.tabnak.ir/fa/archive?service_id=-1&sec_id=-1&cat_id=-1&rpp=20&from_date=1384/01/01&to_date=1401/03/24&p={page}'
         html = requests.get(page_url).text
         soap = BeautifulSoup(html, 'lxml')
         links = soap.findAll("div", {"class": "linear_news"})
-        # links = soap.find_all('h3')
         page += 1
         for link in tqdm(links):
             news_url = 'https://www.tabnak.ir'+link.a['href']
@@ -26,7 +24,7 @@ def scrapAll():
                 article.download()
                 article.parse()
             except:
-                print('fuck')
+                print('download failed')
             scraptedData.append({
                 'url': news_url,
                 'text': article.text,
@@ -34,12 +32,8 @@ def scrapAll():
             })
         if page == int(pagesNum) + 1:
             break
-        # print(links[2])
-
-        # print(len(links))
     df = pd.DataFrame(scraptedData)
     df.to_csv(f'{fileName}.csv')
-    # print(scraptedData)
 
 
 scrapAll()
